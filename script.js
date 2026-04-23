5b3fad8d1073267107c677e50e65b4aadb191c1d
waltermoseki <walter@example.com>
2026-04-23 10:07:49 +0200

    Updated content

--- script.js
+++ script.js
@@ -163,3 +163,20 @@ function showResult() {
         document.getElementById("result").innerHTML = "You have NORMAL skin.";
     }
 }
+
+
+/*******menubar*******/
+
+let menulist = document.getElementById("menulist")
+menulist.style.maxHeight = "0px";
+
+function toggleMenu() {
+    if (
+        menulist.style.maxHeight == "0px") {
+
+        menulist.style.maxHeight = "200px";
+    } 
+    else {
+        menulist.style.maxHeight = "0px";
+    }
+}
